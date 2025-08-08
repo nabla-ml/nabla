0b1fc2074d110e6c93b6f075054a826d54e49b44
TilliFe <tillmann.fehrenbach@gmail.com>
2025-08-08 10:29:28 +0000

    Remove unused file.

--- test.py
+++
@@ -1,7 +0,0 @@
-# verify_jax.py
-import jax
-try:
-    print(f"JAX backend: {jax.default_backend()}")
-    print(f"JAX devices: {jax.devices()}")
-except Exception as e:
-    print(f"An error occurred: {e}")
