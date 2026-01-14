8afab918ab0f858aa3361037f3dd7998c96db8ae
TilliFe <tillmann.fehrenbach@gmail.com>
2026-01-14 20:38:40 +0100

    chore: Delete the `run_all_tests.sh` script.

--- run_all_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running all tests via pytest..."
-# Run all tests in the verified directories
-python3 -m pytest tests/unit/sharding/ tests/integration/with_sharding/ tests/integration/no_sharding/ -v
-
-echo "All tests passed!"
